a4abcea6f7b9e09e9d90367a2d662a8ce0747882
akshay mordiya <akshaymordiya9506@gmail.com>
2024-07-04 19:08:11 +0530

    contact page form reconstructed and added mail sending service

---
+++ src/app/(profile)/contact/Schema/index.js
@@ -0,0 +1,21 @@
+import * as Yup from 'yup';
+
+export const validationSchema = Yup.object().shape({
+  name: Yup.string()
+    .required('Name is required')
+    .min(2, 'Name must be at least 2 characters long')
+    .max(30, 'Name cannot be longer than 50 characters'),
+  company: Yup.string()
+    .required('Company is required')
+    .min(2, 'Company must be at least 2 characters long')
+    .max(30, 'Company cannot be longer than 50 characters'),
+  email: Yup.string()
+    .email('Invalid email format')
+    .required('Email is required'),
+  comment: Yup.string()
+    .required('Comment is required')
+    .min(20, 'Comment must be at least 20 characters long')
+    .max(700, 'Comment cannot be longer than 500 characters'),
+  categoryTags: Yup.array().min(1, 'At least one category tag is required'),
+  priceRange: Yup.string().required('Price range is required'),
+});
---
+++ src/app/components/Alert/index.jsx
@@ -0,0 +1,67 @@
+"use client";
+import React, { useState, useMemo, useEffect } from 'react';
+import { createPortal } from 'react-dom';
+
+//icons
+import TaskAltIcon from '@mui/icons-material/TaskAlt';
+import WarningAmberIcon from '@mui/icons-material/WarningAmber';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+
+import "./index.scss";
+
+const BASE_CLASSNAME = "alert";
+
+const icons = {
+  "info": InfoOutlinedIcon,
+  "success": TaskAltIcon,
+  "error": WarningAmberIcon
+}
+
+const Alert = ({
+  show = false,
+  type = "",
+  text = "",
+  onClose,
+  duration = 20000
+}) => {
+
+  const [isClosing, setIsClosing] = useState(false);
+
+  const Icon = useMemo(() => {
+    if(type === ""){
+      return () => <React.Fragment></React.Fragment>
+    }
+
+    return icons[type]
+  }, [type]);
+
+  const handleClose = () => {
+    setIsClosing(true);
+    setTimeout(() => {
+      onClose();
+      setIsClosing(false);
+    }, [2000]);
+  }
+
+  useEffect(() => {
+    if(show){
+      setTimeout(() => {
+        handleClose();
+      }, duration)
+    }
+  }, [show, duration]);
+
+  return createPortal(
+    <div className={`${BASE_CLASSNAME} ${type} ${(show && !isClosing)  ? "open": "close" }`}>
+      <div className={`${BASE_CLASSNAME}_left`}>
+        <Icon className="icon" />
+        <p>{text}</p>
+      </div>
+      <HighlightOffIcon className={`${BASE_CLASSNAME}_right`} onClick={handleClose} />
+    </div>,
+    document.getElementById("alert-box")
+  )
+}
+
+export default Alert
