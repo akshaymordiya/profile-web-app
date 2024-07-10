eb5236d047e8f2a2d21db649c4693ec40b7cff9c
akshay mordiya <akshaymordiya9506@gmail.com>
2024-07-10 17:31:38 +0530

    common footer reconstructed

--- src/app/components/Footer/index.jsx
+++ src/app/components/Footer/index.jsx
@@ -2,136 +2,90 @@ import React from "react";
 import "./index.scss";
 
 //components
+import Link from "next/link";
 import IMG from "../IMG";
 import Grid from "../Grid";
 import SectionBox from "../SectionBox";
-import footer from "../../../data/global.footer.json";
-import { renderIconComponent } from "@/Icons";
-import Link from "next/link";
-import FooterwidgetTitle from "./FooterWidgetitle";
 import Section from "../Section";
+import FooterwidgetTitle from "./FooterWidgetitle";
+//utils
+import { renderIconComponent } from "@/Icons";
+//data
+import footer from "../../../data/footer.json";
+
+const BASE_CLASSNAME = "footer-cm_container";
 
 const Footer = () => {
-  const BASE_CLASSNAME = "footer-container";
   return (
-    <footer>
-      <div className="footer-main-wrapper">
-        <div className="footer-area">
+    <footer className={BASE_CLASSNAME}>
+      <IMG 
+        src={footer.images.shape}
+        useRawImgTag
+        imageClasses={`${BASE_CLASSNAME}_shape_img`}
+      />
+      <Grid classNames={`${BASE_CLASSNAME}_grid`}>
+        <Grid.Item
+          sm={12}
+          xs={12}
+          md={6}
+          lg={3}
+          xlg={3}
+          itemClass={`${BASE_CLASSNAME}_grid_col-1`}
+        >
           <IMG
-            src={footer.footerImg}
+            src={footer.images.logo}
             useRawImgTag
-            alt="footer-img"
-            useContainer
-            containerClasses="footer-shape"
+            imageClasses="logo"
           />
-          <Section>
-            <Grid classNames={`${BASE_CLASSNAME}`}>
-              <Grid.Item
-                xs={12}
-                sm={6}
-                md={8}
-                lg={4}
-                xlg={4}
-                itemClass="footer-widget"
-              >
-                <IMG
-                  src={footer.whitelogo}
-                  useRawImgTag
-                  alt="Logo-white"
-                  useContainer
-                  containerClasses="footer-logo"
-                />
-                <div className="footer-content">
-                  <p>{footer.paragraph}</p>
-                </div>
-                <div className="footer-social">
-                  {footer.socialmedia.map((item, index) => (
-                    <Link key={index} href="/" target="_blank">
-                      <span>{renderIconComponent(item?.icon)}</span>
-                    </Link>
-                  ))}
-                </div>
-              </Grid.Item>
-              <Grid.Item
-                xs={12}
-                sm={6}
-                md={4}
-                lg={2}
-                xlg={2}
-                itemClass="footer-widget"
-              >
-                <SectionBox
-                  itemClass="footer-widget-title"
-                  title={footer.explore.title}
-                />
-                <ul>
-                  {footer.explore.exploredata.map((item, i) => {
-                    return (
-                      <li key={i}>
-                        <Link href="#">
-                          {/* <Arrowforward /> */}
-                          {item?.title}
-                        </Link>
-                      </li>
-                    );
-                  })}
-                </ul>
-              </Grid.Item>
-              <Grid.Item
-                xs={12}
-                sm={6}
-                md={8}
-                lg={3}
-                xlg={3}
-                itemClass="footer-widget"
-              >
-                <SectionBox
-                  itemClass="footer-widget-title"
-                  title={footer.address.title}
-                />
-                <FooterwidgetTitle
-                  parentclass="footer-widget-address"
-                  title={footer.address.subtitle}
-                  parag={footer.address.paragraph}
-                />
-              </Grid.Item>
-              <Grid.Item
-                xs={12}
-                sm={6}
-                md={4}
-                lg={3}
-                xlg={3}
-                itemClass="footer-widget"
-              >
-                <SectionBox
-                  itemClass="footer-widget-title"
-                  title={footer.contact.title}
-                />
-                <FooterwidgetTitle
-                  parentclass="footer-widget-mail"
-                  title={footer.contact.email}
-                />
-                <FooterwidgetTitle
-                  parentclass="footer-widget-mail"
-                  title={footer.contact.number}
-                />
-              </Grid.Item>
-            </Grid>
-          </Section>
-        </div>
-        <Grid classNames="copyright-bdr-top">
+          <p>{footer.paragraph}</p>
+          <div className="social_media_container">
+            {footer.socialMedia.map(s => (
+              <Link 
+                key={s.title} className="social_media_container_link"
+                href={s.link}
+              >     {renderIconComponent(s. icon)}
+              </Link>
+            ))}
+          </div>
+        </Grid.Item>
+        {Object.entries(footer.grid).map(([key,value]) => (
           <Grid.Item
+            key={key}
             sm={12}
             xs={12}
-            md={12}
-            lg={12}
-            xlg={12}
-            itemClass="copyright-text"
-            data-speed="0.5"
+            md={6}
+            lg={3}
+            xlg={3}
+            itemClass={`${BASE_CLASSNAME}_grid_col-menu`}
           >
-            <span>{footer.copyright.title}</span>
+            <div className={`${BASE_CLASSNAME}_grid_col-menu_box`}>
+              <h2 className={`${BASE_CLASSNAME}_grid_col-menu_box_title`}>{value.title}</h2>
+              <ul className={`${BASE_CLASSNAME}_grid_col-menu_box_items_list`}>
+                {value.data.map((d, index) => (
+                  <li key={`${key}_${index}`} className={`${BASE_CLASSNAME}_grid_col-menu_box_items_list_item`}>
+                    {d?.link ? (
+                      <Link
+                        href={d.link}
+                        className="link"
+                      >
+                        {renderIconComponent(d.icon)}
+                        <span>{d.title}</span>
+                      </Link>
+                    ) : (
+                      <div className="text">
+                        {renderIconComponent(d.icon)}
+                        <span>{d.title}</span>
+                      </div>
+                    )}
+                  </li>
+                ))}
+              </ul>
+            </div>
           </Grid.Item>
-        </Grid>
+        ))}
+      </Grid>
+      <div className={`${BASE_CLASSNAME}_copyright`}>
+        <span>{footer.copyright.title}</span>
       </div>
     </footer>
   );
